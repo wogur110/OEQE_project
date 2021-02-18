5d20b0f22466e518b702ee583ceb51882b8f90f2
BaeJaehyeok <wogur110@gmail.com>
2021-02-18 16:18:40 +0900

    modified pupil_to_realsense_with_calibrate.py

--- pupil_to_realsense_with_calibrate.py
+++ pupil_to_realsense_with_calibrate.py
@@ -94,10 +94,9 @@ np.save('./convert_matrix',convert_matrix)
 
 def make_convert_matrix():
     """
-    각도기->148도가 정면이고 1방향 회전만 할 수 있게 처리해 놨으니까 하드코딩해 둠
-    (x_1, y_1, z_1): world coordinate와 유사 (realsense 좌표계와 평행하지만 안구 중심이 원점임)
-    (x_2, y_2, z_2): viewing coordinate (pupil coordinate)
-    앞으로 pupil coordinate에서 
+    front side = input degree is 184
+    (x_1, y_1, z_1): similar to world coordinate 
+    (x_2, y_2, z_2): viewing coordinate (pupil coordinate) 
     """
     coord_1 = np.array()
     X_2 = np.array()
@@ -119,7 +118,7 @@ def make_convert_matrix():
         theta_2 = message_1[b'theta']
         phi_2 = message_1[b'phi']
 
-        x_2 = np.sin(theta_2) * np.cos(phi_2) #이게맞나..
+        x_2 = np.sin(theta_2) * np.cos(phi_2)
         y_2 = np.cos(theta_2)
         z_2 = np.sin(theta_2) * np.sin(phi_2)
 
@@ -149,70 +148,76 @@ def convert_pupil_to_realsense(theta, phi) :
     
     return converted_theta, converted_phi
 
+if __name__ == "__main__":
+    need_calculate = raw_input("Start Calculating?(Y/N) : ")
+    if (need_calculate == "Y" or "y") :
+        make_convert_matrix()
 
-# Start Pupil_to_Realsense
+    print(convert_matrix)
+    raw_input("Start Pupil_to_Realsense(press enter)")
+    
+    # Start Pupil_to_Realsense
+    try:
+        while True:
+            current_time = time.time()
 
-try:
-    while True:
-        current_time = time.time()
+            # Collect Data from pupil_tracker &  Wait for a coherent pair of frames: depth and color
+            sub_1_3d.connect(b"tcp://%s:%s" %(addr.encode('utf-8'),sub_port))
+            topic,msg_1 =  sub_1_3d.recv_multipart() # pupil tracker (maximum 120Hz)
+            
+            frames = pipeline.wait_for_frames() # realsense (maximum 30Hz)
 
-        # Collect Data from pupil_tracker &  Wait for a coherent pair of frames: depth and color
-        sub_1_3d.connect(b"tcp://%s:%s" %(addr.encode('utf-8'),sub_port))
-        topic,msg_1 =  sub_1_3d.recv_multipart() # pupil tracker (maximum 120Hz)
-        
-        frames = pipeline.wait_for_frames() # realsense (maximum 30Hz)
+            message_1 = loads(msg_1)
+            theta = message_1[b'theta']
+            phi = message_1[b'phi']
+            
+            depth_frame = frames.get_depth_frame()
+            color_frame = frames.get_color_frame()
+            if not depth_frame or not color_frame:
+                continue
 
-        message_1 = loads(msg_1)
-        theta = message_1[b'theta']
-        phi = message_1[b'phi']
-        
-        depth_frame = frames.get_depth_frame()
-        color_frame = frames.get_color_frame()
-        if not depth_frame or not color_frame:
-            continue
+            # Convert images to numpy arrays
+            depth_image = np.asanyarray(depth_frame.get_data())
+            color_image = np.asanyarray(color_frame.get_data())
 
-        # Convert images to numpy arrays
-        depth_image = np.asanyarray(depth_frame.get_data())
-        color_image = np.asanyarray(color_frame.get_data())
+            # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
+            depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
 
-        # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
-        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+            # Check tracking point on images
+            converted_theta, converted_phi = convert_pupil_to_realsense(theta, phi)
 
-        # Check tracking point on images
-        converted_theta, converted_phi = convert_pupil_to_realsense(theta, phi)
+            H,W = color_image.shape[0], color_image.shape[1]
+            point_y = int(H/2 + H/2 * (np.tan(converted_theta) / np.tan(COLOR_CAMERA_MAX_THETA)))
+            point_x = int(W/2 + W/2 * (np.tan(converted_phi) / np.tan(COLOR_CAMERA_MAX_PHI)))
+            point_y = np.clip(point_y, 0, H-1)
+            point_x = np.clip(point_x, 0, W-1)  
+            color_image = cv2.line(color_image, (point_x, point_y), (point_x, point_y), red_color, 5)
 
-        H,W = color_image.shape[0], color_image.shape[1]
-        point_y = int(H/2 + H/2 * (np.tan(converted_theta) / np.tan(COLOR_CAMERA_MAX_THETA)))
-        point_x = int(W/2 + W/2 * (np.tan(converted_phi) / np.tan(COLOR_CAMERA_MAX_PHI)))
-        point_y = np.clip(point_y, 0, H-1)
-        point_x = np.clip(point_x, 0, W-1)  
-        color_image = cv2.line(color_image, (point_x, point_y), (point_x, point_y), red_color, 5)
+            H,W = depth_colormap.shape[0], depth_colormap.shape[1]
+            point_y = int(H/2 + H/2 * (np.tan(converted_theta) / np.tan(DEPTH_CAMERA_MAX_THETA)))
+            point_x = int(W/2 + W/2 * (np.tan(converted_phi) / np.tan(DEPTH_CAMERA_MAX_PHI)))
+            point_y = np.clip(point_y, 0, H-1)
+            point_x = np.clip(point_x, 0, W-1)        
+            depth_colormap = cv2.line(depth_colormap, (point_x, point_y), (point_x, point_y), red_color, 5)
+            text = "depth : " + str(depth_image[point_y][point_x]) + "mm"
+            depth_colormap = cv2.putText(depth_colormap, text, (10,30), cv2.FONT_HERSHEY_SIMPLEX, 1, red_color, 2)
+            print(round(current_time - current_time_0, 4), theta, phi)
+            print(point_x, point_y, depth_image[point_y][point_x])
 
-        H,W = depth_colormap.shape[0], depth_colormap.shape[1]
-        point_y = int(H/2 + H/2 * (np.tan(converted_theta) / np.tan(DEPTH_CAMERA_MAX_THETA)))
-        point_x = int(W/2 + W/2 * (np.tan(converted_phi) / np.tan(DEPTH_CAMERA_MAX_PHI)))
-        point_y = np.clip(point_y, 0, H-1)
-        point_x = np.clip(point_x, 0, W-1)        
-        depth_colormap = cv2.line(depth_colormap, (point_x, point_y), (point_x, point_y), red_color, 5)
-        text = "depth : " + str(depth_image[point_y][point_x]) + "mm"
-        depth_colormap = cv2.putText(depth_colormap, text, (10,30), cv2.FONT_HERSHEY_SIMPLEX, 1, red_color, 2)
-        print(round(current_time - current_time_0, 4), theta, phi)
-        print(point_x, point_y, depth_image[point_y][point_x])
+            # Stack both images horizontally
+            images = np.hstack((color_image, depth_colormap))
 
-        # Stack both images horizontally
-        images = np.hstack((color_image, depth_colormap))
+            # Show images
+            cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
+            cv2.imshow('RealSense', images)
+            cv2.waitKey(1)
 
-        # Show images
-        cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
-        cv2.imshow('RealSense', images)
-        cv2.waitKey(1)
+            sub_1_3d.disconnect(b"tcp://%s:%s" %(addr.encode('utf-8'),sub_port))
 
+
+    finally:
+        # Stop streaming
+        pipeline.stop()
+        #sub.disconnect(b"tcp://%s:%s" %(addr.encode('utf-8'),sub_port))
         sub_1_3d.disconnect(b"tcp://%s:%s" %(addr.encode('utf-8'),sub_port))
-
-
-finally:
-    # Stop streaming
-    pipeline.stop()
-    #sub.disconnect(b"tcp://%s:%s" %(addr.encode('utf-8'),sub_port))
-    sub_1_3d.disconnect(b"tcp://%s:%s" %(addr.encode('utf-8'),sub_port))
 
